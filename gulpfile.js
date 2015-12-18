4d8e7d7027479fd34078d9ca05ffa39674070c41
Yeondam Park <giyen9088@gmail.com>
2015-12-17 23:53:46 -0500

    flappy-birds

--- gulpfile.js
+++ gulpfile.js
@@ -10,59 +10,64 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var neat = require('node-neat').includePaths;
+
+var paths = {
+    scss: './assets/sass/*.scss'
+};
 
 // JavaScript linting task
-gulp.task('jshint', function() {
-  return gulp.src('site/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+gulp.task('jshint', function () {
+    return gulp.src('site/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 // Compile Sass task
-gulp.task('sass', function() {
-  return gulp.src('site/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('site/css'));
+gulp.task('sass', function () {
+    return gulp.src('site/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('site/css'));
 });
 
 // Watch task
-gulp.task('watch', function() {
-  gulp.watch('site/js/*.js', ['jshint']);
-  gulp.watch('site/scss/*.scss', ['sass']);
+gulp.task('watch', function () {
+    gulp.watch('site/js/*.js', ['jshint']);
+    gulp.watch('site/scss/*.scss', ['sass']);
 });
 
 // Default task
 gulp.task('default', ['jshint', 'sass', 'watch']);
 
 // Minify index
-gulp.task('html', function() {
-  return gulp.src('site/index.html')
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('build/'));
+gulp.task('html', function () {
+    return gulp.src('site/index.html')
+        .pipe(minifyHTML())
+        .pipe(gulp.dest('build/'));
 });
 
 // JavaScript build task, removes whitespace and concatenates all files
-gulp.task('scripts', function() {
-  return browserify('site/js/main.js')
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
+gulp.task('scripts', function () {
+    return browserify('site/js/main.js')
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
 });
 
 // Styles build task, concatenates all the files
-gulp.task('styles', function() {
-  return gulp.src('site/css/*.css')
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('build/css'));
+gulp.task('styles', function () {
+    return gulp.src('site/css/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('build/css'));
 });
 
 // Image optimization task
-gulp.task('images', function() {
-  return gulp.src('site/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('build/img'));
+gulp.task('images', function () {
+    return gulp.src('site/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'));
 });
 
 // Build task
